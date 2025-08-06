386a6020436e3b1e02dd5e5b7cb392a5f2df81f2
Abhinav Bhati <abhati68@uwo.ca>
2025-08-05 20:54:59 -0400

    Inital Launch

---
+++ home.js
@@ -0,0 +1,27 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+// Set Home as active on initial load
+document.querySelector('.nav-link[href="home.html"]').classList.add('active');
+
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
