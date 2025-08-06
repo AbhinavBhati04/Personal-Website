393d1899987194534e4d938a9d60357691500153
Abhinav Bhati <abhati68@uwo.ca>
2025-08-06 11:34:41 -0400

    Mobile View Fix

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ window.addEventListener("scroll", () => {
   });
 });
 
+// Toggle slide-out mobile menu
+function toggleMenu() {
+  const menu = document.getElementById("sideMenu");
+  menu.style.width = (menu.style.width === "250px") ? "0" : "250px";
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").style.width = "0";
+}
+
